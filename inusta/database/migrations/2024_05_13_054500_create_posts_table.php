7b780f8b0b07c2ea1cdfbb3f43513d68cb21766b
yoshikawa risa <yoshikawarisa@yoshikawanonotobukkukonpyuta.local>
2024-05-16 14:22:28 +0900

    “ユーザーログイン機能実装完了”

--- inusta/database/migrations/2024_05_13_054500_create_posts_table.php
+++ inusta/database/migrations/2024_05_13_054500_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable(false);
+            $table->text('text')->nullable(false);
+            $table->text('user_id')->nullable(false);
             $table->timestamps();
         });
     }
