af3d0a80ea735ef9196ecf00de9f6d7539a065c4
yoshikawa risa <yoshikawarisa@yoshikawanonotobukkukonpyuta.local>
2024-05-21 16:40:48 +0900

    “ホーム画面の修正”

--- inusta/app/Http/Controllers/UsersController.php
+++ inusta/app/Http/Controllers/UsersController.php
@@ -58,5 +58,11 @@ class UsersController extends Controller
         ]);
     }
 
-
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
+    }
 }
