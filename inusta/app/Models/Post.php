fe96b4f629efe708ea8e461906c3380c7977aa3f
yoshikawa risa <yoshikawarisa@yoshikawanonotobukkukonpyuta.local>
2024-05-24 11:58:33 +0900

    新規投稿機能実装

--- inusta/app/Models/Post.php
+++ inusta/app/Models/Post.php
@@ -8,5 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    
+
+    // モデルが関連付けられるテーブル名
+    protected $table = 'posts';
+
+    // 可変項目
+    protected $fillable = ['text','photo','user_id'];
+
+    //PostモデルがUserモデルに属している
+    public function user()
+    {
+        return $this->belongsTo(Post::class); 
+    }
+
 }
--- inusta/routes/web.php
+++ inusta/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\QuestionsController;
 use App\Http\Controllers\QuestionCommentsController;
 use App\Http\Controllers\PostsController;
 
+//ユーザー機能
 Route::get('/', [UsersController::class, 'index'])->name('users.index');  //登録の画面の話
 Route::get('/users/create', [UsersController::class, 'create'])->name('users.create');  //登録の画面の話
 Route::post('/users', [UsersController::class, 'store'])->name('users.store'); //登録のバック-処理の話
@@ -16,7 +17,7 @@ Route::get('/login', [UsersController::class, 'loginForm'])->name('login');
 Route::post('/login', [UsersController::class, 'login'])->name('users.login'); 
 Route::post('/logout', [UsersController::class, 'logout'])->name('logout');
 
-
+//犬プロフィール機能
 Route::get('/dogs/index', [DogsController::class, 'index'])->name('dogs.index'); //犬一覧
 Route::get('/dogs/create', [DogsController::class, 'create'])->name('dogs.create');
 Route::post('/dogs', [DogsController::class, 'store'])->name('dogs.store'); //登録のバック-処理の話
@@ -24,6 +25,7 @@ Route::get('/dogs/{id}/edit', [DogsController::class, 'edit'])->name('dogs.edit'
 Route::put('/dogs/{id}', [DogsController::class, 'update'])->name('dogs.update'); //登録のバック-処理の話
 Route::get('/dogs/{id}/show', [DogsController::class, 'show'])->name('dogs.show');  //登録の画面の話
 
+//質問機能
 Route::get('/questions/index', [QuestionsController::class, 'index'])->name('questions.index'); //質問一覧
 Route::get('/questions/create', [QuestionsController::class, 'create'])->name('questions.create');
 Route::post('/questions', [QuestionsController::class, 'store'])->name('questions.store'); //登録のバック-処理の話
@@ -31,7 +33,11 @@ Route::get('/questions/{id}/show', [QuestionsController::class, 'show'])->name('
 Route::get('/questions/{id}/edit', [QuestionsController::class, 'edit'])->name('questions.edit');  //登録の画面の話
 Route::put('/questions/{id}', [QuestionsController::class, 'update'])->name('questions.update'); //登録のバック-処理の話
 
+//質問ーコメント機能
 Route::post('/questions/{questionId}/comments', [QuestionCommentsController::class, 'store'])->name('question_comments.store'); 
 Route::delete('/comments/{id}', [QuestionCommentsController::class, 'destroy'])->name('question_comments.destroy');  //削除の話
 
-Route::get('/posts/index', [PostsController::class, 'index'])->name('posts.index'); //質問一覧
+//投稿機能
+Route::get('/posts/index', [PostsController::class, 'index'])->name('posts.index'); //一覧
+Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostsController::class, 'store'])->name('posts.store'); //登録のバック-処理の話
